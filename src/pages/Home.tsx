dd80f5f2510419fd31ff1e6ba1bec24cc7c21c8e
namiDev <namiDev@gmail.com>
2026-09-06 11:55:57 +0700

    menambahkan animation image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,14 @@
 const Home = () => {
   return (
-    <div>
-      <div className="flex flex-col pt-10 relative overflow-hidden h-screen">
-        <img src="https://png.pngtree.com/png-vector/20250126/ourmid/pngtree-soft-whites-clouds-on-transparent-background-png-image_15341810.png" alt="awan" className="animate-cloude-infinite absolute w-200 top-0" style={{ animationDuration: '50s'}}/>
-        <img src="https://png.pngtree.com/png-vector/20250126/ourmid/pngtree-soft-whites-clouds-on-transparent-background-png-image_15341810.png" alt="awan" className="animate-cloude-infinite absolute w-200 top-80" style={{ animationDuration: '45s'}}/>
-        <p className="text-[150px] lg:text-[95px] font-poppins text-gray-500 font-extrabold text-center">
+    <div className="relative overflow-hidden h-full">
+      <img src="/images/Awan.png" alt="awan" width={2000} className="animate-cloude-infinite absolute top-0" style={{ animationDuration: '100s' }} />
+      <div className="flex flex-col pt-20">
+
+        {/* <img src="https://png.pngtree.com/png-vector/20250126/ourmid/pngtree-soft-whites-clouds-on-transparent-background-png-image_15341810.png" alt="awan" className="animate-cloude-infinite absolute w-200 top-80" style={{ animationDuration: '30s'}}/> */}
+        <p className="text-[150px] lg:text-[95px] font-poppins text-white font-extrabold text-center">
           Cek Prakiraan Cuaca
         </p>
-        <span className="z-10 text-[150px] lg:text-[95px] font-poppins text-blue-400 font-extrabold text-center">Indonesia </span>
+        <span className="z-10 text-[150px] lg:text-[95px] font-poppins font-extrabold text-center"><span className="text-white">Indo</span><span className="text-red-500">nesia</span></span>
 
         <p className="text-[20px] font-google-sans font-medium text-center text-white">
           Kamu bisa cek prakiraan cuaca dari Kelurahan / Desa tempat kamu tinggal, sekarang hingga 2 hari kedepan
